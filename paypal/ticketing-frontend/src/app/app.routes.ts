3c4a82d8ebb611ed921f8e81868f085e1a48d5be
Dev <200591365@student.georgianc.on.ca>
2025-12-10 01:28:28 -0500

    updated code

--- paypal/ticketing-frontend/src/app/app.routes.ts
+++ paypal/ticketing-frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SuccessComponent } from './success/success.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { MyTicketsComponent } from './my-tickets/my-tickets.component';
 
-
 export const routes: Routes = [
   { path: '', redirectTo: 'events', pathMatch: 'full' },
   { path: 'events', component: EventsComponent },
   { path: 'ticket/:id', component: TicketComponent },
   { path: 'my-tickets', component: MyTicketsComponent },
-  { path: 'success', component: SuccessComponent }
+  { path: 'success', component: SuccessComponent },
+
+  // ⭐ VERY IMPORTANT FOR RENDER DEPLOYMENT ⭐
+  { path: '**', redirectTo: 'events' }
 ];
